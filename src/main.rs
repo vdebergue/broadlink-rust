c86e6cc7576a1e822b3213b0bd85dfb16acd3109
Vincent Debergue <v.debergue@samsung.com>
2018-03-16 17:59:27 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::{UdpSocket, Ipv4Addr, IpAddr};
+use std::net::{UdpSocket, Ipv4Addr, IpAddr, SocketAddr};
 use std::time::Duration;
 extern crate time;
 
@@ -21,12 +21,13 @@ fn get_local_ip() -> IpAddr {
 }
 
 fn discover(local_ip: Ipv4Addr, timeout: Option<Duration>) {
-  let socket = UdpSocket::bind("0.0.0.0:0").expect("bind failed");
+  let socket = UdpSocket::bind(SocketAddr::new(IpAddr::V4(local_ip), 0)).expect("bind failed");
   socket.set_read_timeout(timeout).expect("set_read_timeout failed");
   socket.set_broadcast(true).expect("set_broadcast failed");
 
   let addr = socket.local_addr().expect("Could not get local addr");
   let packet = hello_packet(local_ip, addr.port());
+  println!("sending packet {:?} on socket {:?}", &packet[0..0x30], socket);
   socket.send_to(&packet, "255.255.255.255:80").expect("couldn't send data");
 
   let mut buf = [0; 1024];
@@ -39,8 +40,19 @@ fn hello_packet(local_ip: Ipv4Addr, port: u16) -> [u8; 0x30] {
     let now = time::now();
     let timezoneOffset: i32 = now.tm_utcoff / 3600 * -1;
     let mut packet: [u8; 0x30] = [0; 0x30];
-    write_u32_le(&mut packet, timezoneOffset as u32, 0x08);
-    write_u16_le(&mut packet, (now.tm_year + 1900) as u16, 0x0c);
+    if (timezoneOffset < 0) {
+        packet[0x08] = (0xff + (timezoneOffset) - 1) as u8;
+        packet[0x09] = 0xff;
+        packet[0x0a] = 0xff;
+        packet[0x0b] = 0xff;
+    } else {
+        packet[0x08] = timezoneOffset as u8;
+    }
+    // write_u32_le(&mut packet, timezoneOffset as u32, 0x08);
+    let year = (now.tm_year + 1900);
+    packet[0x0c] = (year & 0xff) as u8;
+    packet[0x0d] = (year >> 8) as u8;
+    // write_u16_le(&mut packet, (now.tm_year + 1900) as u16, 0x0c);
     // packet[0x0e] = now.tm_sec as u8;
     packet[0x0e] = now.tm_min as u8;
     packet[0x0f] = now.tm_hour as u8;
@@ -52,7 +64,7 @@ fn hello_packet(local_ip: Ipv4Addr, port: u16) -> [u8; 0x30] {
     packet[0x19] = local_ip.octets()[1];
     packet[0x1a] = local_ip.octets()[2];
     packet[0x1b] = local_ip.octets()[3];
-    packet[0x1c] = port as u8;
+    packet[0x1c] = (port & 0xff) as u8;
     packet[0x1d] = (port >> 8) as u8;
     packet[0x26] = 6;
     let mut checksum: i32 = 0xbeaf;
@@ -60,8 +72,9 @@ fn hello_packet(local_ip: Ipv4Addr, port: u16) -> [u8; 0x30] {
     for i in 0..packet.len() {
         checksum += packet[i] as i32;
     }
-    write_u16_le(&mut packet, (checksum & 0xffff) as u16 ,0x20);
-
+    packet[0x20] = (checksum & 0xff) as u8;
+    packet[0x21] = (checksum >> 8) as u8;
+    // write_u16_le(&mut packet, (checksum & 0xffff) as u16 ,0x20);
     packet
 }
 
